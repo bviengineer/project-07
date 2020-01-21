3f87f24ef52554f7585263c480777d1b02948fd7
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-01-21 17:01:32 -0500

    src/settings.php > add api settings
    add API settings

--- php_rest_api/src/settings.php
+++ php_rest_api/src/settings.php
@@ -13,5 +13,11 @@ return [
             'name' => 'slim-app',
             'path' => __DIR__ . '/../logs/app.log',
         ],
+
+        // API
+        'api' => [
+            'version' => 'v1',
+            'base_url' => 'http://localhost:8888',
+        ],
     ],
 ];
