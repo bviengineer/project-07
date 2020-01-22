a80bfbecf46fcf1df32a4405b56005d59d8740bc
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-01-22 13:34:05 -0500

    routes.php
    Add code to retireve data as Json

--- php_rest_api/src/routes.php
+++ php_rest_api/src/routes.php
@@ -3,8 +3,11 @@
 
 $app->get('/[{name}]', function ($request, $response, $args) {
     // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+    // $this->logger->info("Slim-Skeleton '/' route");
 
     // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+    // return $this->renderer->render($response, 'index.phtml', $args);
+
+    $result = $this->todo->getTodos(); // here todo is referring to the container?
+    return $response->withJson($result, 200, JSON_PRETTY_PRINT);
 });
