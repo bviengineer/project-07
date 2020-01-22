9b4b914c2eb6a50a86451ea0d0cb4b10d043b2f8
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-01-22 12:15:10 -0500

    php_rest_api/src/dependencies.php > remove white space & dbase attribute mode
    remove white space
    add dbase attribute mode

--- php_rest_api/src/dependencies.php
+++ php_rest_api/src/dependencies.php
@@ -29,6 +29,6 @@ $container['db'] = function ($c) {
     $db = $c->get('settings')['db'];
     $pdo = new PDO($db['dsn'].':'.$db['database']);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE: Set);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     return $pdo;
 };
