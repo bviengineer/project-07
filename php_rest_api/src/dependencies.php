3ef2266a13d37db1c3c53965763593b3271e34b8
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-01-22 13:17:55 -0500

    Update dependencies.php
    Add container for Todo model

--- php_rest_api/src/dependencies.php
+++ php_rest_api/src/dependencies.php
@@ -32,3 +32,8 @@ $container['db'] = function ($c) {
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     return $pdo;
 };
+
+// Todo model container 
+$container['todos'] = function($c) {
+    return new App\Model\Todo($c->get('db'));
+};
