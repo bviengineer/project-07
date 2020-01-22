74c590d18c8c930b6c0567f12ab864b9b737e872
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-01-22 13:18:28 -0500

    Update todos.php
    Add dbase construct and function to retrieve list of todos

--- php_rest_api/src/Model/todos.php
+++ php_rest_api/src/Model/todos.php
@@ -1,1 +1,20 @@
 <?php
+namespace App\Model;
+
+class Todo 
+{
+    protected $db;
+
+    public function __construct(\PDO $database)
+    {
+        $this->db = $database;
+    }
+
+    public function getTodos() 
+    {
+        $sqlStmt = 'SELECT * FROM tasks ORDER BY id';
+        $results = $this->db->prepare($sqlStmt);
+        $results->execute();
+        return $results->fetchAll();
+    }
+}
