5dbcee0d10057d46b1a09f7b0eed8cb3db73069e
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-01-23 13:48:14 -0500

    Model/Todo.php > add method to add todo
    Add method to create a todo to Todo.php class

--- php_rest_api/src/Model/Todo.php
+++ php_rest_api/src/Model/Todo.php
@@ -26,4 +26,14 @@ class Todo
         $result->execute();
         return $result->fetch();
     }
+    // Creates a Todo
+    public function createTodo($todoId) 
+    {
+        $sqlStmt = 'INSERT INTO tasks(task, status) VALUES(:task, :status)';
+        $result = $this->db->prepare($sqlStmt);
+        $result->bindParam(':task', $todoId['task'], PDO::PARAM_STR);
+        $result->bindParam(':id', $todoId['status'], PDO::PARAM_INT);
+        $result->execute();
+        return $this-getTodo($this->db->lastInsertId());
+    }
 }
