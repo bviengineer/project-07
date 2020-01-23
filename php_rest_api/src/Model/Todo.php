925e12a898c75ed8eff9970a37b0b85af8725611
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-01-23 14:20:28 -0500

    Update Todo.php
    -Add a method to delete a todo
    -Add missing > where neded
    -Remove a 's' from Create a Todo comment

--- php_rest_api/src/Model/Todo.php
+++ php_rest_api/src/Model/Todo.php
@@ -26,7 +26,7 @@ class Todo
         $result->execute();
         return $result->fetch();
     }
-    // Creates a Todo
+    // Create a Todo
     public function createTodo($todo) 
     {
         $sqlStmt = 'INSERT INTO tasks(task, status) VALUES(:task, :status)';
@@ -34,7 +34,7 @@ class Todo
         $result->bindParam(':task', $todo['task'], PDO::PARAM_STR);
         $result->bindParam(':id', $todo['status'], PDO::PARAM_INT);
         $result->execute();
-        return $this-getTodo($this->db->lastInsertId());
+        return $this->getTodo($this->db->lastInsertId());
     }
     // Update a Todo
     public function updateTodo($todo) 
@@ -45,6 +45,15 @@ class Todo
         $result->bindParam(':task', $todo['task'], PDO::PARAM_STR);
         $result->bindParam(':id', $todo['status'], PDO::PARAM_INT);
         $result->execute();
-        return $this-getTodo($todo['id']);
+        return $this->getTodo($todo['id']);
+    }
+    // Delete a Todo
+    public function deleteTodo($todoId) 
+    {
+        $sqlStmt = 'DELETE FROM tasks WHERE id = :id';
+        $result = $this->db->prepare($sqlStmt);
+        $result->bindParam(':id', $todoId, PDO::PARAM_INT);
+        $result->execute();
+        return ['message' => 'That item has been removed from the list'];
     }
 }
