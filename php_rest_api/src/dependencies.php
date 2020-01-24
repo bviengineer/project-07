77c70d9fa197cebd6041974021b0c9e7cfcd7424
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-01-24 17:24:25 -0500

    src/dependencies.php > add api url setting
    add settings for the api url

--- php_rest_api/src/dependencies.php
+++ php_rest_api/src/dependencies.php
@@ -21,6 +21,7 @@ $container['logger'] = function ($c) {
 // API container
 $container['api'] = function ($c) {
     $api = $c->get('settings')['api'];
+    $api['api_url'] = $api['base_url'] . '/api/' . $api['version'];
     return $api;
 };
 
