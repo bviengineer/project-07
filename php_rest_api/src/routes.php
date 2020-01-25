d70348ad18cd6ddebf68346659a3f4b9aae17d18
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-01-24 19:00:04 -0500

    src/routes.php
    -Update list of endpoints
    -Remove commented-out code
    -Add comments to routes nested inside of route group
    - Add route to create a todo

--- php_rest_api/src/routes.php
+++ php_rest_api/src/routes.php
@@ -3,35 +3,41 @@
 
 $app->get('/', function ($request, $response, $args) {
     $endpoints = [
-        'list of todos' => $this->api['api_url'].'/todos',
-        'single todo' => $this->api['api_url'].'/todos/{todoId}',
-        'todos by status' => $this->api['api_url'].'/todos/status',
-        'single status' => $this->api['api_url'].'/todos/status_id',
+        'list all todos' => $this->api['api_url'].'/todos',
+        'list a single todo' => $this->api['api_url'].'/todos/{todoId}',
+        'add a todo' => $this->api['api_url'].'/todos', // needed
+        'update a todo' => $this->api['api_url'].'/todos/todoId',
         'help' => $this->api['base_url'].'/'
     ];
-
     $result = [
         'endpoints' => $endpoints,
         'version' => $this->api['version'],
         'datetime' => date('c')
     ];
-
-    //$result = $this->todo->getTodos(); // here todo is referring to the container?
     return $response->withJson($result, 200, JSON_PRETTY_PRINT);
 });
-// [GET] /api/v1/todos
-// [POST] /api/v1/todos
-// [GET] /api/v1/todos/{id}
-// [PUT] /api/v1/todos/{id}
-// [DELETE] /api/v1/todos/{id}
 $app->group('/api/v1/todos', function() use ($app){
+    // List all todos
     $app->get('', function ($request, $response, $args) {    
-        $result = $this->todo->getTodos(); // here todo is referring to the container?
+        $result = $this->todo->getTodos(); 
         return $response->withJson($result, 200, JSON_PRETTY_PRINT);
     });
-    $app->get('/{todoId}', function ($request, $response, $args) {    
+    // List a specific todo
+    $app->get('/todoId', function ($request, $response, $args) {    
         $result = $this->todo->getTodo($args['todoId']);
-        var_dump($result);
-        //return $response->withJson($result, 200, JSON_PRETTY_PRINT);
+        //var_dump($result);
+        return $response->withJson($result, 200, JSON_PRETTY_PRINT);
+    });
+    // Create a todo
+    $app->post('', function ($request, $response, $args) {    
+        $result = $this->todo->createTodo($request->getParsedBody());
+        //var_dump($result);
+        return $response->withJson($result, 201, JSON_PRETTY_PRINT);
     });
 });
+
+// [GET] /api/v1/todos > DONE
+// [POST] /api/v1/todos
+// [GET] /api/v1/todos/{id} DONE
+// [PUT] /api/v1/todos/{id}
+// [DELETE] /api/v1/todos/{id}
